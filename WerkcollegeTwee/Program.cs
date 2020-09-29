0940e9c198a5e45726e8ad72f93b12b49742aa76
Shrai Singh <shrai.singh@gmail.com>
2020-09-29 11:43:43 +0200

    Add project files.

---
+++ WerkcollegeTwee/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using WerkcollegeTweeLib;
+
+namespace WerkcollegeTwee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rekening = new Account("BE23 2938 3838 9393", 1000, 2.0);
+
+            while (true)
+            {
+                Console.WriteLine(rekening);
+                Console.WriteLine("Acties: [D]eposit, [W]ithdraw");
+                var input = Console.ReadKey();
+                if (input.Key == ConsoleKey.D)
+                {
+                    Console.WriteLine("\nAmount to deposit:");
+                    var amount = Console.ReadLine();
+                    rekening.Deposit(double.Parse(amount));
+                }
+                else if (input.Key == ConsoleKey.W)
+                {
+                    Console.WriteLine("\nAmount to withdraw:");
+                    var amount = Console.ReadLine();
+                    rekening.Withdraw(double.Parse(amount));
+                }
+                else
+                {
+                    Console.WriteLine("Foutieve input");
+                }
+            }
+
+        }
+    }
+}
---
+++ WerkcollegeTweeLib/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WerkcollegeTweeLib
+{
+    public class Account
+    {
+        public Account(string iBAN, double balance, double interest)
+        {
+            this.iban = iBAN;
+            this.balance = balance;
+            this.creationDate = DateTime.Now;
+            this.interest = interest;
+        }
+
+        private string iban { get; set; }
+        private double balance { get; set; }
+        private DateTime creationDate { get; set; }
+        private double interest { get; set; }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if ((balance - amount) > -1000)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                throw new Exception("Balance too low, maximum overdraft of 1000");
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"Account: { iban } with balance { balance } generating { interest }% interest made on { creationDate }";
+        }
+
+    }
+}
